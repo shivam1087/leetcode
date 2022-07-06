e93418751403a2e8110dc8acdb16a8cd0d2b7ed9
shivam1087 <108589413+shivam1087@users.noreply.github.com>
2022-07-06 20:38:27 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (80.05%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -3,26 +3,20 @@ public:
     int fib(int n) 
     {
         
-        int f=0,s=1,ans=0;
+        int first=0,second=1,ans=0;
         if(n==0)
             return 0;
         else
             if(n==1)
-                return 1;
-        // else if(n==2)
-        //     return 1;
-        else
-        {
-        
+            return 1;
         for(int i=2;i<=n;i++)
         {
-            ans=f+s;
+            ans=first+second;
         
-            f=s;
-            s=ans;
+            first=second;
+            second=ans;
         }
         return ans;
-        }
         
     }
 };
