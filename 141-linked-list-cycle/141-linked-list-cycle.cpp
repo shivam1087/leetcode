407629b6dcfa6ee4f13ad8552ff08bc803c86152
shivam1087 <108589413+shivam1087@users.noreply.github.com>
2022-07-11 03:36:50 +0530

    Time: 12 ms (76.06%), Space: 8.1 MB (16.54%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -12,19 +12,14 @@ public:
     {
         ListNode* slow=head;
         ListNode* fast=head;
-        
         while(fast!=NULL and fast->next!=NULL)
         {
             slow=slow->next;
             fast=fast->next->next;
             
             if(slow==fast)
-                return true;
-            
+                return true;  
         }
         return false;
-    }
-        
-        
-        
+    }      
 };
