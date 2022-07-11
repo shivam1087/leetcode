610c2ba3d3a5118dd3d09cbff2e13d5054d030b1
shivam1087 <108589413+shivam1087@users.noreply.github.com>
2022-07-11 15:45:17 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (32.11%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     vector<vector<int>> generate(int numrows) 
     {
-        
-        vector<vector<int>> pascal(numrows);
+       vector<vector<int>> pascals(numrows);
         for(int i=0;i<numrows;i++)
         {
-            pascal[i].resize(i+1);
-            pascal[i][0]=pascal[i][i]=1;
-            
+            pascals[i].resize(i+1);
+            pascals[i][0]=pascals[i][i]=1;
             for(int j=1;j<i;j++)
             {
-                pascal[i][j]=pascal[i-1][j]+pascal[i-1][j-1];
+                pascals[i][j]=pascals[i-1][j]+pascals[i-1][j-1];
             }
         }
-        return pascal;
+        return pascals;
+       
         
     }
 };
