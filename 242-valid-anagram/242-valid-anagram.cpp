ef4ebbea8820b8c4be84adc798e8f51b6b2cdb2b
shivam1087 <108589413+shivam1087@users.noreply.github.com>
2022-07-12 12:41:39 +0530

    Time: 6 ms (90.57%), Space: 7.4 MB (45.56%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -6,8 +6,8 @@ public:
     
         int n=s.size();
         int m=t.size();
-        //vector<int>v(n,0);
-         int v[26];
+        vector<int>v(26,0);
+         //int v[26];
         for(int i=0;i<26;i++)
         {
             v[i]=0;
